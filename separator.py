63181ecbb2638d0463db9c5ceb6fb3211a046a02
Sang <81050372+sang-san@users.noreply.github.com>
2023-09-11 00:52:29 +0200

    Update separator.py
    small changes

--- separator.py
+++ separator.py
@@ -4,8 +4,9 @@ from itertools import takewhile
 
 
 class Pricelist_Separator_Middleware:
-    def __init__(self, print_runtime: bool = True):
+    def __init__(self, print_runtime: bool = True, print_lenghts: bool = False):
         self.print_runtime = print_runtime
+        self.print_lenghts = print_lenghts
 
     def filter_for_lowest_value_keys(self, dct: dict):
         if dct:
@@ -56,8 +57,7 @@ class Pricelist_Separator_Middleware:
 
                 for sku in newly_added_skus: local_sku_spots_granted.pop(sku, None)
 
-
-            print(f"pricelist for {steamid} had {len(steamid_sku_lists[steamid])} skus assigned, having a set max space of {set_pricelist_space}")
+            if self.print_lenghts: print(f"pricelist for {steamid} had {len(steamid_sku_lists[steamid])} skus assigned, having a set max space of {set_pricelist_space}")
 
         if self.print_runtime: print(f"Runtime: {datetime.now()-start}")
         return steamid_sku_lists
